56904f845b6add623c69b8d401988719e49810cc
tmvorisek <tmvorisek@alaska.edu>
2018-02-08 16:53:22 -0900

    restructured code, and got flask serving static css/js files (Not recommended, but simple).

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-from flask import Flask 
+from flask import Flask, render_template, send_from_directory
 from flask_socketio import SocketIO, send
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 app.config['SECRET_KEY'] = 'GreatBigSecret'
 socketio = SocketIO(app)
 
+@app.route('/css/<path:path>', methods=['GET'])
+def send_css(path):
+  return send_from_directory('css',path)
+
+@app.route('/js/<path:path>', methods=['GET'])
+def send_js(path):
+  return send_from_directory('js',path)
+
+@app.route('/')
+def index():
+  return render_template("./index.html")
+
+
 @socketio.on('message')
 def handleMessage(msg):
-  print('Message: ' + msg)
+  # print(msg)
   send(msg, broadcast=True)
 
 if __name__ == '__main__':
-  socketio.run(app)
+  socketio.run(app, debug = True)
