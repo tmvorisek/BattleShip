2fb762ee93ca749e1ac5202f999f9530c224db1c
tmvorisek <tmvorisek@alaska.edu>
2018-02-09 18:32:55 -0900

    setting up for interface dump

--- js/battleship.js
+++ js/battleship.js
@@ -1,15 +1,19 @@
 $(document).ready(function() {
   var socket = io.connect('http://127.0.0.1:5000');
+  
   socket.on('connect', function() {
   });
+
   socket.on('message', function(msg) {
     console.log(msg)
     $("#messages").append('<li><b>'+msg.name+':</b> '+msg.message+'</li>');
   });
+
   $('#sendbutton').on('click', function() {
     socket.send({
       name: $('#name').val(),
-      message:$('#message').val()
+      message:$('#message').val(),
+      type:"chat"
     });
   });
 });
