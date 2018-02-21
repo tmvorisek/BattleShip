27b8fd81667cb88290d678063415134fc9206605
tmvorisek <tmvorisek@alaska.edu>
2018-02-20 18:41:25 -0900

    Interface chat improvements, begun ship placement and board highlighting.

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, send_from_directory
 from flask_socketio import SocketIO, send
+from datetime import time
 
 import unittest
+
+from battleship import Ship
+from battleship import BattleshipGame
 
 
 app = Flask(__name__, static_url_path='')
 app.config['SECRET_KEY'] = 'GreatBigSecret'
 socketio = SocketIO(app)
+message_history = []
 
 @app.route('/css/<path:path>', methods=['GET'])
 def send_css(path):
@@ -20,12 +25,22 @@ def send_js(path):
 def index():
   return render_template("./index.html")
 
+@socketio.on('connect')
+def handleConnection():
+  for msg in message_history:
+    send(msg)
 
 @socketio.on('message')
 def handleMessage(msg):
-  if( msg["type"] == "chat" ):
-    msg["message"] = msg["message"].replace("<", " I'm am Haxxoer! ")
-    send(msg, broadcast=True)
+  global message_history
+  msg["message"] = msg["message"].replace("<", " I'm am Haxxoer! ")
+  msg["name"] = msg["name"].replace("<", " I'm am Haxxoer! ")
+  
+  if( msg["type"] == "chat" 
+    and msg["message"] != ""
+    and len(msg["message"]) < 240):
+      send(msg, broadcast=True)
+      message_history.append({"name":msg["name"], "message":msg["message"], "type":"chat"})
   print(msg)
 
 if __name__ == '__main__':
